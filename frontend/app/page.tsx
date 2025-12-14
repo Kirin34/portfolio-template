81862a5115d0001375fd3f3a665b6c47f683573a
Davide Manfredi <davidemanfredi2@gmail.com>
2025-12-14 20:17:49 +0100

    fix: change strapi collection tipe from services to Service

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/Header";
 
 
 export default async function Home() {
-  const services = (await fetchFromStrapi("Service")) || [];
+  const services = (await fetchFromStrapi("service")) || [];
 
   return (
     
